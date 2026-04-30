7b3fef08831ea6b4c1f262bc8be28cef5a79b083
Mr-VVM0309 <vovamm79@gmail.com>
2026-04-30 12:09:50 +0300

    First commit

---
+++ Base.py
@@ -0,0 +1,74 @@
+import pygame 
+
+
+clock = pygame.time.Clock()
+
+pygame.init()
+screen = pygame.display.set_mode((1920, 1080), flags=pygame.NOFRAME)
+pygame.display.set_caption("No start No end")
+icon = pygame.image.load('images/icon.png').convert_alpha()
+pygame.display.set_icon(icon)
+
+bg = pygame.image.load('floors/floor mainwood.jpg').convert()
+walk_right = [ pygame.image.load('images walk/r1.png').convert_alpha(),
+              pygame.image.load('images walk/r2.png').convert_alpha()]
+walk_left = [ pygame.image.load('images walk/l1.png').convert_alpha(),
+              pygame.image.load('images walk/l2.png').convert_alpha()]
+
+tank = pygame.image.load('images/tank.png').convert_alpha()
+tank_x = 620
+player_anim_count = 0
+bg_x =0 
+bg_y =0
+
+player_speed = 10
+player_x = 300
+player_y = 350
+walk_sound = pygame.mixer.Sound('sounds/steps.mp3')
+
+walk_sound.play()
+running = True
+while running:
+    
+    screen.blit(bg, (bg_x, bg_y))
+    screen.blit(bg, (bg_x + 2000, bg_y))
+    screen.blit(bg, (bg_x, bg_y + 2000))
+    screen.blit(bg, (bg_x, bg_y - 2000))
+    screen.blit(tank, (tank_x, 300))
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+         screen.blit(walk_left[int(player_anim_count)], (player_x, player_y))
+    else:
+        screen.blit(walk_right[int(player_anim_count)], (player_x, player_y))
+    
+    if keys[pygame.K_LEFT] and player_x > 300:
+        player_x -= player_speed
+        bg_x +=10
+    elif keys[pygame.K_RIGHT] and player_x < 1600:
+        player_x += player_speed
+        bg_x -=10
+    
+    if keys[pygame.K_UP] and player_y > 120:
+        player_y -= player_speed
+        bg_y +=10
+    elif keys[pygame.K_DOWN] and player_y < 900:
+        player_y += player_speed
+        bg_y -=10
+    if player_anim_count >1:
+        player_anim_count = 0
+    else:
+        player_anim_count = player_anim_count + 0.125
+        
+    tank_x -=3.3
+    
+    pygame.display.update()
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+    
+    
+    clock.tick(30)
